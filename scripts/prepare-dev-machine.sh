fec2ae1fd8ce9fec5e834ca2bc3c772a66e1001d
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2018-08-18 11:06:10 +0200

    Move dependency installation to playbooks
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- scripts/prepare-dev-machine.sh
+++ scripts/prepare-dev-machine.sh
@@ -6,15 +6,15 @@ OS_RELEASE=$(grep --no-filename ^NAME /etc/*-release | awk -F '[="]' '/^NAME=/ {
 
 # install dependencies for running OS
 if [ "${OS_RELEASE}" == "Arch Linux" ]; then
-  sudo pacman -S --noconfirm python-pip ansible libvirt qemu vagrant gcc make bridge-utils dmidecode dnsmasq ebtables pkg-config
-  sudo systemctl enable libvirtd.service
-  sudo systemctl start libvirtd.service
-  vagrant plugin install pkg-config
+  sudo pacman -S --noconfirm python-pip ansible # libvirt qemu vagrant gcc make bridge-utils dmidecode dnsmasq ebtables pkg-config
+#  sudo systemctl enable libvirtd.service
+#  sudo systemctl start libvirtd.service
+#  vagrant plugin install pkg-config
 
   # workaround from https://wiki.archlinux.org/index.php/Vagrant#vagrant-libvirt
-  CONFIGURE_ARGS='with-ldflags=-L/opt/vagrant/embedded/lib with-libvirt-include=/usr/include/libvirt with-libvirt-lib=/usr/lib' \
-  GEM_HOME=~/.vagrant.d/gems GEM_PATH=$GEM_HOME:/opt/vagrant/embedded/gems PATH=/opt/vagrant/embedded/bin:$PATH \
-  vagrant plugin install vagrant-libvirt
+#  CONFIGURE_ARGS='with-ldflags=-L/opt/vagrant/embedded/lib with-libvirt-include=/usr/include/libvirt with-libvirt-lib=/usr/lib' \
+#  GEM_HOME=~/.vagrant.d/gems GEM_PATH=$GEM_HOME:/opt/vagrant/embedded/gems PATH=/opt/vagrant/embedded/bin:$PATH \
+#  vagrant plugin install vagrant-libvirt
 
 elif [ "${OS_RELEASE}" == "Fedora" ]; then
   sudo dnf install -y python-pip python2-dnf ansible
