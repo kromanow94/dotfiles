ff9621ecc61b8f57cc8f2158985c969f2c65de0d
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2018-08-20 11:16:25 +0000

    Generalize base installation
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

---
+++ scripts/base-packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e 
+
+DOTFILES_DIR=$(git rev-parse --show-toplevel)
+OS_RELEASE=$(grep --no-filename ^NAME /etc/*-release | awk -F '[="]' '/^NAME=/ { print $3 }')
+
+# install dependencies for running OS
+if [ "${OS_RELEASE}" == "Arch Linux" ]; then
+  sudo pacman -S --noconfirm python-pip ansible
+
+elif [ "${OS_RELEASE}" == "Fedora" ]; then
+  sudo dnf install -y python-pip ansible
+
+elif [ "${OS_RELEASE}" == "CentOS Linux" ]; then
+  sudo dnf install -y python-pip ansible
+fi
+
