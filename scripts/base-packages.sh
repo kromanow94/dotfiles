cfe9dfb1f84d32646ea36645b7f5b559a87e863b
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2018-08-20 14:21:38 +0000

    Install python2
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- scripts/base-packages.sh
+++ scripts/base-packages.sh
@@ -6,12 +6,12 @@ OS_RELEASE=$(grep --no-filename ^NAME /etc/*-release | awk -F '[="]' '/^NAME=/ {
 
 # install dependencies for running OS
 if [ "${OS_RELEASE}" == "Arch Linux" ]; then
-  sudo pacman -S --noconfirm python-pip ansible
+  sudo pacman -S --noconfirm python python2 python-pip ansible
 
 elif [ "${OS_RELEASE}" == "Fedora" ]; then
-  sudo dnf install -y python-pip ansible
+  sudo dnf install -y python python2 python-pip ansible
 
 elif [ "${OS_RELEASE}" == "CentOS Linux" ]; then
-  sudo dnf install -y python-pip ansible
+  sudo dnf install -y python python2 python-pip ansible
 fi
 
