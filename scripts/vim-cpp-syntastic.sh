291bac3d9fb3758093a5bf98029fff517f1660a7
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2019-07-12 12:35:54 +0200

    Install syntastic for cpp for Fedora
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

---
+++ scripts/vim-cpp-syntastic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+OS_RELEASE=$(grep --no-filename ^NAME /etc/*-release | awk -F '[="]' '/^NAME=/ { print $2 }')
+
+# install dependencies for running OS
+if [ "${OS_RELEASE}" == "Fedora" ] || [ "${OS_RELEASE}" == "CentOS Linux" ]; then
+    sudo yum install -y cppcheck
+else
+    echo "Script does not handle system: ${OS_RELEASE}"
+fi
+
+pip3 install --user cpplint cppclean
