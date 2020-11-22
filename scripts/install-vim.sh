cbc39f525ee38915e3d21c0f777618afc5292dd8
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2020-11-22 19:16:37 +0000

    Update vim installation
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- scripts/install-vim.sh
+++ scripts/install-vim.sh
@@ -2,10 +2,10 @@
 # configure command:
 $ CFLAGS=-fPIC ./configure --with-features=huge             --enable-multibyte             --enable-rubyinterp=yes             --enable-python3interp=yes             --with-python3-config-dir=$(python3-config --configdir)             --enable-perlinterp=yes             --enable-luainterp=yes             --enable-gui=gtk2             --enable-cscope             --prefix=/usr/local --with-tlib=ncurses --exec-prefix=/
 
-# Fedora packages
-sudo yum install -y ruby ruby-devel lua lua-devel luajit \
-luajit-devel ctags git python python-devel \
-python3 python3-devel tcl-devel \
-perl perl-devel perl-ExtUtils-ParseXS \
-perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
-perl-ExtUtils-Embed
+# # Fedora packages
+# sudo yum install -y ruby ruby-devel lua lua-devel luajit \
+# luajit-devel ctags git python python-devel \
+# python3 python3-devel tcl-devel \
+# perl perl-devel perl-ExtUtils-ParseXS \
+# perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+# perl-ExtUtils-Embed
