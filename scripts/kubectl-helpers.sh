6e46c332dc6ff5c0362ff8f2fd198dbc802a78e6
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2021-11-23 20:07:02 +0100

    Add configs/00-minimal.yaml
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- scripts/kubectl-helpers.sh
+++ scripts/kubectl-helpers.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+if ! command -v kubectl ; then
+    echo Installing kubectl to ~/.local/bin/
+    cd $(mktemp -d)
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    mv kubectl ~/.local/bin/
+fi
+
 echo "Downloading krew archive"
 cd "$(mktemp -d)"
 curl -LO "https://github.com/kubernetes-sigs/krew/releases/download/v0.3.3/krew.{tar.gz,yaml}"
