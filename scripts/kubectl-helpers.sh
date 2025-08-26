9e484ea1e2af41512093580749890fcdfc3487a6
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2025-08-26 08:34:26 +0000

    kubectl completion bash > bash-completion/bash_completion.d/kubectl

--- scripts/kubectl-helpers.sh
+++ scripts/kubectl-helpers.sh
@@ -3,14 +3,18 @@ set -e
 
 if ! command -v kubectl ; then
     echo Installing kubectl to ~/.local/bin/
-    cd $(mktemp -d)
+    TMPDIR=$(mktemp -d)
+    cd "${TMPDIR}"
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod +x kubectl
     mv kubectl ~/.local/bin/
+    cd -
+    rm -rf "${TMPDIR}"
 fi
 
 echo "Downloading krew archive"
-cd "$(mktemp -d)"
+TMPDIR=$(mktemp -d)
+cd "${TMPDIR}"
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
 KREW="krew-${OS}_${ARCH}"
@@ -19,10 +23,11 @@ curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${
 tar zxvf "${KREW}.tar.gz"
 ./"${KREW}" install krew
 set +x
+cd -
+rm -rf "${TMPDIR}"
 
 echo "Configuring kubectl completion"
-mkdir -p ~/.bash_completion.d
-kubectl completion bash > ~/.bash_completion.d/kubectl
+kubectl completion bash > bash-completion/bash_completion.d/kubectl
 
 echo "Updating ~/.bashrc"
 ln -s ~/.local/bin/kubectl ~/.local/bin/k
